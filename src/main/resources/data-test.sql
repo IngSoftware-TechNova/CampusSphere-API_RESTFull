cdda9eed5b6985a4a7e5214e1b80b5fa631b96cd
marco <marck-08-2001@hotmail.com>
2024-09-25 00:44:21 -0500

    expection

--- src/main/resources/data-test.sql
+++ src/main/resources/data-test.sql
@@ -1,6 +1,11 @@
+/*
 INSERT INTO locations (location, city, country) VALUES
                                                       ('Av. José Larco 123', 'Lima', 'Perú'),
                                                       ('Jr. Puno 456', 'Lima', 'Perú'),
                                                       ('Av. Arequipa 789', 'Lima', 'Perú'),
                                                       ('Av. Brasil 1010', 'Lima', 'Perú'),
                                                       ('Jr. Tacna 2020', 'Lima', 'Perú');
+
+
+
+ */
