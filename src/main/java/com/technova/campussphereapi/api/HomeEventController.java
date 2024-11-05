96f384157eeb210c9b77016afd1e6901fa7721e2
J3ffo3 <117779481+J3ffo3@users.noreply.github.com>
2024-11-04 19:25:06 -0500

    feat: evento detalle publico

--- src/main/java/com/technova/campussphereapi/api/HomeEventController.java
+++ src/main/java/com/technova/campussphereapi/api/HomeEventController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,11 @@ public class HomeEventController {
     public ResponseEntity<List<EventDetailsDTO>> getRecentEvents() {
         List<EventDetailsDTO> recentEvents = eventService.findTop8EventsByCreatedAt();
         return new ResponseEntity<>(recentEvents, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EventDetailsDTO> search(@PathVariable("id") Integer id){
+        EventDetailsDTO event = eventService.findById(id);
+        return new ResponseEntity<>(event, HttpStatus.OK);
     }
 }
